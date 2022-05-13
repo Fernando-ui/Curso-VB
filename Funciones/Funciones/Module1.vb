03af79e567e938190215e7ddcd034eb3d8c29d7e
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-05-13 17:03:50 -0500

    'clasess'

---
+++ Funciones/Funciones/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine(sumar(3, 2))
+
+    End Sub
+
+    Function sumar(num1 As Integer, num2 As Integer) As Integer
+        Return num1 + num2
+
+    End Function
+    Function buscarElemento(numBuscar As Integer, vector() As Integer) As Integer
+
+    End Function
+
+
+End Module
--- PruebaMetodos/PruebaMetodos/Module1.vb
+++ PruebaMetodos/PruebaMetodos/Module1.vb
@@ -1,9 +1,23 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Console.WriteLine("Tenemos el procedimiento del primero")
+        mostrarNombre()
+        nombresColocados("Fernando", "Sosa")
+        Dim numero = 3
+        cambiarNumeroVariable(numero)
+        Console.WriteLine(numero)
     End Sub
     Sub mostrarNombre()
         Console.WriteLine("Como se llaman")
+    End Sub
+    Sub nombresColocados(nombre1 As String, nombre2 As String)
+        Console.WriteLine(nombre1, nombre2)
+
+    End Sub
+
+    Sub cambiarNumeroVariable(ByRef numero As Integer)
+        numero = 20
+
     End Sub
 End Module
