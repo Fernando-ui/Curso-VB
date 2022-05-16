afbb49078203852c440fe84ab7b180091e36675a
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-05-16 09:46:16 -0500

    agregando clases

---
+++ Clasess/Clasess/AireAcondicionado.vb
@@ -0,0 +1,21 @@
+﻿Public Class AireAcondicionado
+    Public marca As String
+    Public modelo As String
+    Public color As String
+    Public voltaje As Integer
+    Public btu As Integer
+    Public temperatura As Integer
+    Private contrasenia As String
+
+    Public Sub subirTemperatura()
+        temperatura += 1
+        contrasenia = "asdkjf"
+        Console.WriteLine(contrasenia)
+    End Sub
+    Public Sub bajarTemperatura()
+        temperatura -= 1
+    End Sub
+    Public Function darTemperaturaActual() As Integer
+        Return temperatura
+    End Function
+End Class
--- Clasess/Clasess/Module1.vb
+++ Clasess/Clasess/Module1.vb
@@ -1,7 +1,21 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim elAire As AireAcondicionado = New AireAcondicionado()
+        elAire.color = "azul"
+        elAire.modelo = "no se cual modelo"
+        elAire.marca = "La mejor de todas"
+        elAire.voltaje = 233
+        elAire.btu = 3324
+        elAire.temperatura = 0
 
+        Console.WriteLine(elAire.color)
+        Console.WriteLine(elAire.modelo)
+        Console.WriteLine(elAire.marca)
+        Console.WriteLine(elAire.voltaje)
+        Console.WriteLine(elAire.btu)
+        elAire.subirTemperatura()
+        Console.WriteLine(elAire.temperatura)
     End Sub
 
 End Module
