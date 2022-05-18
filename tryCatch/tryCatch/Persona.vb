7da487aac792e2597d7bab8e3ee36172c72569f4
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-05-18 15:22:52 -0500

    agregando tryCatch

--- tryCatch/tryCatch/Persona.vb
+++ tryCatch/tryCatch/Persona.vb
@@ -8,9 +8,15 @@
         edad = ed
     End Sub
     Public Sub datos(nombre, id, edad)
-        Console.WriteLine(nombre)
-        Console.WriteLine(id)
-        Console.WriteLine(edad)
+        Try
+            Console.WriteLine(nombre)
+            Console.WriteLine(id)
+            Console.WriteLine(edad)
+
+        Catch ex As Exception
+            Console.WriteLine(ex)
+            Console.WriteLine("Tenemos un error con la ejecucion")
+        End Try
     End Sub
 
 End Class
--- tryCatch/tryCatch/Program.vb
+++ tryCatch/tryCatch/Program.vb
@@ -4,18 +4,22 @@ Module Program
     Sub Main()
         Dim nom, id As String
         Dim edad As Integer
+        Try
+            Console.WriteLine("Ingresa el nombre de la persona")
+            nom = Console.ReadLine
 
-        Console.WriteLine("Ingresa el nombre de la persona")
-        nom = Console.ReadLine
 
+            Console.WriteLine("Ingrese el numero de identificacion de la persona")
+            id = Console.ReadLine
 
-        Console.WriteLine("Ingrese el numero de identificacion de la persona")
-        id = Console.ReadLine
+            Console.WriteLine("Ingrese la edad de la persona")
+            edad = Console.ReadLine
 
-        Console.WriteLine("Ingrese la edad de la persona")
-        edad = Console.ReadLine
+            Dim p As New Persona(nom, id, edad)
+            p.datos(nom, id, edad)
 
-        Dim p As New Persona(nom, id, edad)
-        p.datos(nom, id, edad)
+        Catch ex As Exception
+            Console.WriteLine("Tenemos un error")
+        End Try
     End Sub
 End Module
