3a88bdb812fee51713768697afa504d0007837f9
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-05-19 12:20:09 -0500

    agregando ejemplos de listas

--- metodosArrray/metodosArrray/Program.vb
+++ metodosArrray/metodosArrray/Program.vb
@@ -6,9 +6,19 @@ Module Program
         miLista.Add(3)
         miLista.Add(19)
         miLista.Add(4)
-        Console.WriteLine(miLista)
-        For i = 0 To miLista.Count - 1 Step 1
-            Console.WriteLine(miLista.Item(i))
+        'Console.WriteLine(miLista)
+        'For i = 0 To miLista.Count - 1 Step 1
+        '    Console.WriteLine(miLista.Item(i))
+        '    Console.WriteLine(miLista(i))
+        'Next
+        Dim copiaLista() As Integer = miLista.ToArray
+        miLista.Clear()
+        Console.WriteLine(copiaLista)
+        Console.WriteLine("Imprimiendo la lista de arriba")
+
+        For Each elemento As Integer In copiaLista
+            Console.WriteLine(elemento)
+            Console.WriteLine("No tenemos nada")
         Next
     End Sub
 End Module
